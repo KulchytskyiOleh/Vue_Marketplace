1a6713b739c87f96bb2572686cadcdc19017d16f
Oleh Kulchytskyi <olegk@GRT-Oleg.k>
2021-07-29 20:21:08 +0300

    fix home page

---
+++ src/views/data.js
@@ -0,0 +1,88 @@
+// import img from '/src/img'
+const data = [
+  {
+    id: 1,
+    title: 'Bahai',
+    image: '/img/Mask1.png',
+    liked: true,
+    price: '$575.09',
+  },
+  {
+    id: 2,
+    title: 'Zürich',
+    image: '/img/Mask2.png',
+    liked: false,
+    price: '$518.54',
+  },
+  {
+    id: 3,
+    title: 'Londolozi',
+    image: '/img/Mask3.png',
+    liked: false,
+    price: '$185.3',
+  },
+  {
+    id: 4,
+    title: 'Ol Jogi Home',
+    image: '/img/Mask4.png',
+    liked: false,
+    price: '$984.05',
+  },
+  {
+    id: 5,
+    title: 'Los Cabos',
+    image: '/img/Mask5.png',
+    liked: false,
+    price: '$263.08',
+  },
+  {
+    id: 6,
+    title: 'Tarangire National Park',
+    image: '/img/Mask6.png',
+    liked: false,
+    price: '$682.29',
+  },
+  {
+    id: 7,
+    title: 'Dorado',
+    image: '/img/Mask7.png',
+    liked: false,
+    price: '$190.67',
+  },
+  {
+    id: 8,
+    title: 'Maldives',
+    image: '/img/Mask8.png',
+    liked: false,
+    price: '$275.02',
+  },
+  {
+    id: 9,
+    title: "Elewana Elsa's Kopje",
+    image: '/img/Mask9.png',
+    liked: false,
+    price: '$155.54',
+  },
+  {
+    id: 10,
+    title: 'Okavango Delta',
+    image: '/img/Mask10.png',
+    liked: false,
+    price: '$677.22',
+  },
+  {
+    id: 11,
+    title: 'Olare Orok',
+    image: '/img/Mask11.png',
+    liked: false,
+    price: '$219.43',
+  },
+  {
+    id: 12,
+    title: 'Limerick',
+    image: '/img/Mask12.png',
+    liked: false,
+    price: '$346.96',
+  },
+];
+export default data;
